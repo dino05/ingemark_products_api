d264e8bfc01da5191782f336bf993b500dcf838e
dmrkonjic <dino.mrkonjic@gmail.com>
2025-03-07 14:06:52 +0100

    Updated Product model with column names and defining ID field.

--- src/main/java/com/ingemark/products/model/entity/Product.java
+++ src/main/java/com/ingemark/products/model/entity/Product.java
@@ -1,7 +1,6 @@
 package com.ingemark.products.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -10,10 +9,19 @@ import java.math.BigDecimal;
 @Entity
 @Table(name = "product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "code")
     private String code;
+    @Column(name = "name")
     private String name;
+    @Column(name = "price_eur")
     private BigDecimal priceEur;
+    @Column(name = "price_usd")
     private BigDecimal priceUsd;
+    @Column(name = "is_available")
     private boolean isAvailable;
 }
