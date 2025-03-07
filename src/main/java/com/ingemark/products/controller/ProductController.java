9ec9502d09aa490d067b1dbd1e1bf91e6394a0f0
dmrkonjic <dino.mrkonjic@gmail.com>
2025-03-07 14:09:23 +0100

    Updated ProductController

--- src/main/java/com/ingemark/products/controller/ProductController.java
+++ src/main/java/com/ingemark/products/controller/ProductController.java
@@ -1,7 +1,11 @@
 package com.ingemark.products.controller;
 
 import com.ingemark.products.model.dto.ProductDTO;
+import com.ingemark.products.service.ProductService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,18 +14,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/products")
 public class ProductController {
 
+    private ProductService productService;
+
     @GetMapping("/find-by-id/{productId}")
     public ResponseEntity<ProductDTO> findByProductId(@PathVariable Long productId){
-        return ResponseEntity.ok().body(null);
+        ProductDTO productDTO = productService.getProductById(productId);
+        return ResponseEntity.ok().body(productDTO);
     }
 
     @GetMapping("/find-all")
-    public ResponseEntity<ProductDTO> findAll(){
-        return ResponseEntity.ok().body(null);
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
+        Page<ProductDTO> products = productService.getAllProducts(pageable);
+        return ResponseEntity.ok().body(products);
     }
 
-    @PostMapping("/update-cart/{userId}")
-    public ResponseEntity<Void> addCartItems(@RequestBody ProductDTO productDTO){
-        return ResponseEntity.ok().body(null);
+    @PostMapping
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO){
+        ProductDTO createdProduct = productService.createProduct(productDTO);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
     }
 }
