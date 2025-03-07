847fc201f4e7ed4ed245d461c0fff73551c71eb3
dmrkonjic <dino.mrkonjic@gmail.com>
2025-03-07 14:01:21 +0100

    Removed record from DTO because of model mapper usage.

--- src/main/java/com/ingemark/products/model/dto/ProductDTO.java
+++ src/main/java/com/ingemark/products/model/dto/ProductDTO.java
@@ -1,11 +1,21 @@
 package com.ingemark.products.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
 import java.math.BigDecimal;
 
-public record ProductDTO (
-        Long id,
-        String code,
-        String name,
-        BigDecimal priceEur,
-        BigDecimal priceUsd,
-        boolean isAvailable){}
+@Data
+public class ProductDTO {
+    private Long id;
+    @NotBlank(message = "Code is required")
+    @Size(min = 10, max = 10, message = "Code must be exactly 10 characters")
+    private String code;
+    private String name;
+    @DecimalMin(value = "0.0", inclusive = true, message = "Price EUR must be greater than or equal to 0")
+    private BigDecimal priceEur;
+    private BigDecimal priceUsd;
+    @JsonProperty("isAvailable")
+    private boolean isAvailable;
+}
